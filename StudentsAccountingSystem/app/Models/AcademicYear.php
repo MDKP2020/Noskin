731c2b8650332255d1bde1521a3c39f83df0c272
Viktor.Noskin <vitek2012rus@gmail.com>
2020-11-01 10:58:18 +0400

    Добавил базовые модели

---
+++ StudentsAccountingSystem/app/Models/AcademicYear.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AcademicYear extends Model
+{
+    protected $table = 'academic_years';
+    protected $guarded = [];
+    public $timestamps = false;
+}
---
+++ StudentsAccountingSystem/app/Models/GroupPattern.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GroupPattern extends Model
+{
+    protected $table = 'group_patterns';
+    protected $guarded = [];
+    public $timestamps = false;
+}
