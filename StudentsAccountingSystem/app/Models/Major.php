fbea2b4e88081547552a3e6a312b2f0eac6b8be8
Viktor.Noskin <vitek2012rus@gmail.com>
2020-11-01 11:46:10 +0400

    Добавил отношение один к многим для направление и паттернов группы

--- StudentsAccountingSystem/app/Models/Major.php
+++ StudentsAccountingSystem/app/Models/Major.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\GroupPattern;
 
 class Major extends Model
 {
     protected $guarded = [];
     public $timestamps = false;
+
+    public function groupPatterns() {
+        return $this->hasMany(GroupPattern::class);
+    }
 }
