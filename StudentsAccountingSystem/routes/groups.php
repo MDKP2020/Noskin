b22f4a02da0c68ed92ef006fd24f7394ee14023d
Masha Crymova 😢 <marie.ower@gmail.com>
2020-11-25 12:11:03 +0400

    Добавлен шаблон для страницы со списком групп

--- StudentsAccountingSystem/routes/groups.php
+++ StudentsAccountingSystem/routes/groups.php
@@ -3,7 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{yearId}', function ($yearId) {
-    dd('Page: groups for year: ' . $yearId );
+//    dd('Page: groups for year: ' . $yearId );
+
+    $data = [];
+    /*
+        в data нужны:
+        - список уч лет (id, value)
+        - список направлений (id, value)
+        - список курсов (id, value)
+        - список групп за уч год (id, name, status)
+    */
+
+    return view('groups')->with('data', $data);
 });
 
 Route::get('/{yearId}/new', function ($yearId) {
