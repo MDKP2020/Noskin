8418d32b40d7fb68033cc598aefcaadc3997f527
Viktor.Noskin <vitek2012rus@gmail.com>
2020-11-28 14:51:11 +0400

    Вынес поле в отельный компонент

--- StudentsAccountingSystem/resources/views/majors/create.blade.php
+++ StudentsAccountingSystem/resources/views/majors/create.blade.php
@@ -8,24 +8,8 @@
     <h2>Создать направление</h2>
     <form method="post" action="{{route('majors.createFromForm')}}">
         @csrf
-        <div class="form-group">
-            <label for="name">Имя</label>
-            <input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name" value="{{old('name')}}" aria-describedby="validationServerNameFeedback">
-            @error('name')
-            <div id="validationServerNameFeedback" class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="code">Код</label>
-            <input name="code" type="text" class="form-control @error('code') is-invalid @enderror" id="code" value="{{old('code')}}" aria-describedby="validationServerCodeFeedback">
-            @error('code')
-            <div id="validationServerCodeFeedback" class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
+        @include('components.form-fields.text', ['name'=>'name', 'title'=>'Имя'])
+        @include('components.form-fields.text', ['name'=>'code', 'title'=>'Код'])
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 @endsection
