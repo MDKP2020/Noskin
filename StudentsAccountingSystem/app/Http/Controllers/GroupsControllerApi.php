86eca48bb2c6e9c27ea577ad1ac341cd36dc1276
Daniil <umarmungenfuerimmer@gmail.com>
2021-01-16 23:10:12 +0300

    Реализован перевод студентов

--- StudentsAccountingSystem/app/Http/Controllers/GroupsControllerApi.php
+++ StudentsAccountingSystem/app/Http/Controllers/GroupsControllerApi.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AcademicYear;
+use App\Models\GroupsToYear;
 use App\Models\StudentToGroup;
 use Illuminate\Http\Request;
 
@@ -28,10 +30,40 @@ class GroupsControllerApi extends Controller
 
     public function transferStudents(Request $request)
     {
-        if (!empty($request['select'])) {
-            return response()->json(["data" => "transfer success"]);
+        if (empty($request['group_id']) || empty($request['select']) || empty($request['year_id'])) {
+            return response()->json(["data" => "fail"]);
         }
-        return response()->json(["data" => "fail"]);
+        $groupId = $request['group_id'];
+        $yearId = $request['year_id'];
+        $currentYear = AcademicYear::where('id', $yearId)->first();
+        $nextYear = AcademicYear::where('start_year', $currentYear->start_year + 1)->firstOrCreate(['start_year' => $currentYear->start_year + 1]);
+        $nextYear->save();
 
+        $currentGroup = GroupsToYear::where('group_id', $groupId)->where('year_id', $yearId)->first();
+        $nextGroup = GroupsToYear::where('group_id', $groupId)->where('year_id', $nextYear->id)->firstOrCreate([
+                "group_id" => $groupId,
+                "year_id" => $nextYear->id,
+                "grade" => $currentGroup->grade + 1,
+                "expel_reason_id" => 1
+            ]
+        );
+        $nextGroup->save();
+
+        foreach ($request['select'] as $student_id) {
+            $startDate = Utils::createFirstDateFromId($yearId);
+            $student = StudentToGroup::where('group_id', $groupId)->where('student_id', $student_id)->where('start_date', $startDate)->first();
+            $student->next_group = $groupId;
+            $student->end_date = Utils::createLastDate($currentYear);
+
+            $newStudent = new StudentToGroup;
+            $newStudent->start_date = Utils::createFirstDate($nextYear);
+            $newStudent->group_id = $groupId;
+            $newStudent->student_id = $student->student_id;
+
+            $student->save();
+            $newStudent->save();
+        }
+
+        return response()->json(["data" => "transfer success"]);
     }
 }
