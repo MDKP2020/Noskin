3bf6ec8f9c18cddeeb006b7191c64ee715e00ef5
Viktor.Noskin <vitek2012rus@gmail.com>
2021-01-16 14:12:56 +0300

    Удаление связей студента и группы перед удалением студента

--- StudentsAccountingSystem/app/Http/Controllers/StudentsController.php
+++ StudentsAccountingSystem/app/Http/Controllers/StudentsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use App\Models\StudentToGroup;
 use Illuminate\Http\Request;
 
 class StudentsController extends Controller
 {
     public function deleteById(int $id) {
+        StudentToGroup::where('student_id', $id)->delete();
         Student::destroy($id);
         return response()->json(["message" => "ok"]);
     }
